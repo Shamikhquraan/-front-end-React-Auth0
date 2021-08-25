6f57ef1cd0f7dcd96fb038bb381fab34d64dc708
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-25 15:08:35 +0300

    Adding lab013

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import SelectBook from "./components/SelectBook";
 import "./BestBooks.css";
-
 import  Card  from 'react-bootstrap/Card';
 import { withAuth0 } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,7 +16,9 @@ class MyFavoriteBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [] ,
+      showData:false,
+      show:false,
     }
   }
    componentDidMount = async ()=> {
@@ -25,17 +27,75 @@ class MyFavoriteBooks extends React.Component {
     let emailaddress = user.email;
     console.log('email',emailaddress);
 
+    try {
 
     let bookData = await axios.get(`${process.env.REACT_APP_SERVER}/getBooks?email=${emailaddress}`);
 
 
+    bookData? await this.setState({
+      books: bookData.data ,
+      showData: true,
+
+    }) : this.setState({
+      books: [] ,
+      showData: false,
+
+    }) }catch(error){
     await this.setState({
-      books: bookData.data
-    })
+        books: [] ,
+        showData: false,
+  
+      })
+
+    }
     
     console.log(this.state.books);
   }
 
+
+
+  handleAddBook  = async (e) =>{
+    console.log(e.target.status.value);
+    e.preventDefault();
+
+    let books ={
+      status : e.target.status.value,
+      description : e.target.desc.value,
+      email : e.target.email.value,
+      title : e.target.title.value,
+    }
+
+    let bookData = await axios.post(`${process.env.REACT_APP_SERVER}/addBook?email=${this.props.auth0.user.email}`,books);
+    console.log(bookData);
+    await this.setState({
+      books: bookData.data,
+    });
+  
+  }
+
+
+  handelShowModel= async ()=>{ 
+    console.log(this);
+    await this.setState({
+      show:true,
+    });
+
+  }
+ handleClose=async ()=>{
+    await this.setState({
+      show:false,
+    })
+  }
+
+  deleteBook = async (bookID) =>{
+  
+    let bookData= await axios.delete(`${process.env.REACT_APP_SERVER}/deletBook/${bookID}?email=${this.props.auth0.user.email}`);
+    console.log(bookData.data);
+    this.setState({
+      books: bookData.data,
+    });
+    
+  }
 
   render() {
     
@@ -46,16 +106,17 @@ class MyFavoriteBooks extends React.Component {
           <p>
             This is a collection of my favorite books
           </p>
+          <button onClick={this.handelShowModel}> Add New Book</button>
 
         </Jumbotron>
         <div className='tab2'>
-        {this.state.books.map((element,key) => {
+        {this.state.showData && this.state.books.map((element,key) => {
           return(
           <Card key ={key} style={{ width: '18rem' }}>
             <Card.Body>
             <img
             className="d-block w-100"
-            src={element.url}
+            src='https://www.concrete-online.co.uk/wp-content/uploads/2020/07/dark-gloomy-books-pages-preview.jpg'
             alt={element.title}
           />
               <Card.Title>{element.title}</Card.Title>
@@ -69,10 +130,14 @@ class MyFavoriteBooks extends React.Component {
               {element.email}
               </Card.Text>
             </Card.Body>
-          </Card>);
+            <h3 className="favorites"> 📧email: </h3>
+          <button onClick={()=>this.deleteBook(element._id)}>Delete</button>
+          </Card>)
         })
         }
         </div>
+        <SelectBook show={this.state.show} handleClose={this.handleClose} handleAddBook={this.handleAddBook} />
+
       </>
     )
   }
